5bd699288fb51d5f9033b8bfd0685807b3c7bfbe
Bob Liu (Akagi201) <akagi201@gmail.com>
2019-02-20 11:05:37 +0800

    feat(kin): modify stellar-core git path

--- kin/core/install.sh
+++ kin/core/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # deploy stellar-core binary
-git clone --depth 1 --branch ${STELLAR_CORE_TAG} https://github.com/kinecosystem/stellar-core /stellar-core \
+git clone --depth 1 --branch ${STELLAR_CORE_TAG} https://github.com/akfork/stellar-core /stellar-core \
   && cd /stellar-core \
   && git submodule init \
   && git submodule update \
